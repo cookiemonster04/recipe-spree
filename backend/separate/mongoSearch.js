b7e136f7373f8980c53dcbc85652ec10a59f9c80
AlexanderThaik <55565946+AlexanderThaik@users.noreply.github.com>
2023-03-02 14:12:33 -0800

    Finished search in MongoDB
    search function takes a list of wanted and unwanted ingredients as inputs
    Recommended recipes are stored under the "Recommended" model in the DB
    Do some command like Recommended.find({}, function (err, count)... to get full list

--- backend/separate/mongoSearch.js
+++ backend/separate/mongoSearch.js
@@ -1,10 +1,13 @@
+//search function takes a list of wanted and unwanted ingredients as inputs
+//Recommended recipes are stored under the "Recommended" model in the DB
+//Do some command like Recommended.find({}, function (err, count)... to get full list
 const fs = require('fs');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const {Recipe, Recommended} = require('./recipeSchema.js');
 const connectDB = require('../connectDb.js');
 
-testDesired = ["chicken", "potato"];
+testDesired = ["chicken", "potato", "pepper"];
 testUndesired = ["onion"];
 
 search(testDesired, testUndesired);
@@ -15,8 +18,22 @@ async function search(desired, undesired)
     await Recommended.deleteMany({})
         .then(() => console.log('Existing recipes cleared'))
         .catch(error => console.error(error));
-    //Looping through array of desired ingredients
-    await desired.forEach(element =>
+    await addDesired(desired);
+    function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms));}
+    //Don't search for more than 1 second, just get whatever recipes can be found in 1 second
+    wait(1000).then(() => {
+        removeUndesired(undesired);
+    });
+    await Recipe.find().sort({ score: -1 });
+    //Add extra delay to prevent DB from closing before search is done
+    wait(5000).then(() => {
+        mongoose.connection.close();
+        console.log("Connection closed");
+    });
+}
+async function addDesired(desired, callback)
+{
+    desired.forEach(element =>
         {
             const regex = new RegExp(element, 'i');
             //Finds all matching recipes for one ingredient
@@ -26,25 +43,31 @@ async function search(desired, undesired)
                 //Looping through each matched recipe
                 matches.forEach(function(match) 
                 {
-                    //Checks if the recipe already has already been added
-                    Recommended.findOne({id: match.id}, function (err, recipe) 
-                    {   
-                        if (!recipe)
-                        {
-                            const newMatch = new Recommended(match.toObject());
-                            newMatch.score = 1;
-                            newMatch.save();
-                            //console.log("match.id");
-                        }
-                        else
-                        {
-                            recipe.score += 1;
-                        }
-                    });
+                    updateOrCreate(match);
                 });
             });
         });
-    //mongoose.connection.close();
-    console.log("closed");
 }
+async function updateOrCreate(match) 
+{
+    //Checks if the recipe already has already been added
+    const filter = {id: match.id};
+    const update = {$inc: {score: 1}, $setOnInsert: match};
+    const options = { new: true, upsert: true, runValidators: true };
+    const result = await Recommended.findOneAndUpdate(filter, update, options);
+    return result;
+}
+async function removeUndesired(undesired)
+{
+    await undesired.forEach(element =>
+        {
+            const regex = new RegExp(element, 'i');
+            console.log(regex);
+            Recommended.deleteMany({"ingredients.text": regex}, function(err, result) {
+                if (err) throw err;
+                console.log(result.deletedCount + " documents deleted");
+            });
+        });
+}
+
 module.exports = search;
