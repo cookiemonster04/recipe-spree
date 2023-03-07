0613129055a7657bc3e685355172aaba88386931
daicon <noahscheff@gmail.com>
2023-03-07 13:49:58 -0800

    Preliminary api handling of recipe information

--- backend/handlers/recipeHandler.js
+++ backend/handlers/recipeHandler.js
@@ -1,9 +1,18 @@
 import path from "path";
+const {a, b, c, d, getRatingInformation} = require("../separate/recipeFunctions.js");
 const CWD = process.cwd();
+const getRecipe = async (req, res, next) => {
+  const reqRecipe = req.params.recipeId;
+  console.log("reqRecipe: " + reqRecipe);
+  const foundRecipe = getRatingInformation(reqRecipe);
+  console.log("found recipe:");
+  console.log(foundRecipe);
+  res.status(201).json(foundRecipe);
+};
 const retrieveRecipe = (req, res, next) => {
   const reqId = req.params.recipeId;
   console.log(`reqid: ${reqId}`);
   res.sendFile(path.join(CWD, `backend/separate/individual/${reqId}.json`));
 };
 
-export default retrieveRecipe;
+export {getRecipe, retrieveRecipe};
