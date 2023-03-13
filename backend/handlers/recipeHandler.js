5a3141081a160285b78026669986bb6da0189d04
AlexanderThaik <55565946+AlexanderThaik@users.noreply.github.com>
2023-03-12 19:19:13 -0700

    Updated recipe handler to return object from MongoDB

--- backend/handlers/recipeHandler.js
+++ backend/handlers/recipeHandler.js
@@ -1,9 +1,17 @@
 import path from "path";
 const CWD = process.cwd();
+const {Recipe, Recommended} = require('./recipeSchema.js');
 const retrieveRecipe = (req, res, next) => {
   const reqId = req.params.recipeId;
   console.log(`reqid: ${reqId}`);
-  res.sendFile(path.join(CWD, `backend/separate/individual/${reqId}.json`));
+  Recipe.find({id: reqID}, function (err, searchedRecipe) { 
+    if (err) throw err; 
+    res.status(200).json({
+      message: "Success",
+      recipe: searchedRecipe,
+    });
+  });
+  //res.sendFile(path.join(CWD, `backend/separate/individual/${reqId}.json`));
 };
 
 export default retrieveRecipe;
