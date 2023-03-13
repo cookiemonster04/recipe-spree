b6fbfb5462800e8e811f5f4a407f6fc733577b10
Noah Cheff <noi@nomad>
2023-03-13 16:47:22 -0700

    Frontend component of recently viewed on profile page

--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -6,50 +6,73 @@ import { Card, CardContent, CardMedia, Typography, Grid} from "@mui/material"
 const Profile = ({ userId, user }) => {
   const [username, setUsername] = useState("");
   const [favlist, setFavlist] = useState([]);
-  const [recipes, setRecipes] = useState([]);
+  const [recentlyViewedList, setRecentlyViewedList] = useState([]);
+  const [favRecipes, setFavRecipes] = useState([]);
+  const [recentRecipes, setRecentRecipes] = useState([]);
 
   useEffect(() => {
+    console.log("a");
     async function getInfo() {
       if (!userId) {
+        console.log("b");
         setUsername(user.username);
         // just to demonstrate it works
         const favInfo = await axios.get(`/api/fav`);
         setFavlist(favInfo.data.favorites);
+        //const rvInfo = await axios.get(`/api/`);
+        setRecentlyViewedList(['0003d5b120']); //replace
       } else {
+        console.log("c");
         const userInfo = await axios.get(`/api/user/${userId}`);
         if (userInfo.hasOwnProperty("data")) {
           setUsername(userInfo.data.username);
         }
         const favInfo = await axios.get(`/api/fav/user/${userId}`);
         setFavlist(favInfo.data.favorites);
+        //const rvInfo = await axios.get(`/api/`);
+        setRecentlyViewedList(['0003d5b120']); //replace
       }
     }
     getInfo();
   }, [userId]);
 
   useEffect(() => {
-    async function getRecipes() {
+    async function getFavRecipes() {
       const recipes = [];
       for (const id of favlist) {
         const recipeInfo = await axios.get(`/api/recipe/${id}`);
         recipes[id] = recipeInfo.data.recipe;
       }
-      setRecipes(recipes);
+      setFavRecipes(recipes);
     }
     if (favlist.length > 0) {
-      getRecipes();
+      getFavRecipes();
     }
   }, [favlist]);
+
+  useEffect(() => {
+    async function getRecentRecipes() {
+      const recipes = [];
+      for (const id of recentlyViewedList) {
+        const recipeInfo = await axios.get(`/api/recipe/${id}`);
+        recipes[id] = recipeInfo.data.recipe;
+      }
+      setRecentRecipes(recipes);
+    }
+    if (recentlyViewedList.length > 0) {
+      getRecentRecipes();
+    }
+  }, [recentlyViewedList]);
 
   return (
     username && (
       <div>
-        <h1>Welcome back, {username}</h1>
+        <h1>Welcome back, {username}.</h1>
         {favlist.length > 0 ? (
           <div>
             <Typography variant="h6">Your favorite recipes:</Typography>
             <Grid container spacing={2}>
-              {Object.values(recipes).map((recipe, idx) => (
+              {Object.values(favRecipes).map((recipe, idx) => (
                 <Grid key={`user_prof_fav_id_${idx}`} item xs={12} sm={6} md={4} lg={3}>
                   <Card sx={{ maxWidth: 400, my: 2 }}>
                     <CardMedia
@@ -76,6 +99,37 @@ const Profile = ({ userId, user }) => {
         ) : (
           <p>You haven't added any favorite recipes yet!</p>
         )}
+      {recentlyViewedList.length > 0 ? (
+        <div>
+        <Typography variant="h6">Your recently viewed recipes:</Typography>
+        <Grid container spacing={2}>
+          {Object.values(recentRecipes).map((recipe, idx) => (
+            <Grid key={`user_prof_fav_id_${idx}`} item xs={12} sm={6} md={4} lg={3}>
+              <Card sx={{ maxWidth: 400, my: 2 }}>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={recipe.image}
+                  alt={recipe.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" sx={{ maxWidth: 400,
+                   overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                    {recipe.title}
+                  </Typography>
+                  {/* <Typography variant="body2" color="text.secondary">
+                    {recipe.ingredients.join(", ")}
+                  </Typography> */}
+                  <Link to={`/recipe/${recentlyViewedList[idx]}`}>View Recipe</Link>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+      ) : (
+        <p>You haven't recently viewed any recipes.</p>
+      )}
       </div>
     )
   );
