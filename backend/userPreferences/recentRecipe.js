751be85f669bb929f31895221cad95d31905f38d
AlexanderThaik <ajt.school@gmail.com>
2023-03-14 17:33:18 -0700

    Implemented recently viewed display

--- backend/userPreferences/recentRecipe.js
+++ backend/userPreferences/recentRecipe.js
@@ -27,8 +27,8 @@ async function recentRecipe(selectedUsername, recipeId)
 }
 
 const recentHandler = catchWrap(async (req, res, next) => {
-    // const { selectedUsername, recipeId } = { ,  };
-    res.status(200).json(await recentRecipe(req.user._id, req.body.recipeId));
+    const { selectedUsername, recipeId } = req.body;
+    res.status(200).json(await recentRecipe(selectedUsername, recipeId));
 });
 
 export { recentRecipe, recentHandler };
--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -11,19 +11,27 @@ const Profile = ({ userId, user }) => {
   const [recentRecipes, setRecentRecipes] = useState([]);
 
   useEffect(() => {
-    console.log("a");
+    //console.log("a");
     async function getInfo() {
       if (!userId) {
-        console.log("b");
+        //console.log("b");
         setUsername(user.username);
         // just to demonstrate it works
         const favInfo = await axios.get(`/api/fav`);
+        console.log("Fav info: ");
+        console.log(favInfo.data);
         setFavlist(favInfo.data.favorites);
+        //setRecentlyViewedList([]);
         //setRecentlyViewedList(['0003d5b120']);
+        //const userInfo = await axios.get(`/api/user`);
         const userInfo = await axios.get(`/api/user`);
-        console.log("recentlyviewed: ");
-        console.log(userInfo.data.recentlyViewed);
+        //const userInfo = await axios.get(`/api/user/${userId}`);
+        console.log("User info: ");
+        console.log(userInfo.data);
         setRecentlyViewedList(userInfo.data.recentlyViewed);
+        //console.log(userInfo.data);
+        //console.log("recentlyviewed: ");
+        //console.log(userInfo.data.recentlyViewed);
       } else {
         console.log("c");
         const userInfo = await axios.get(`/api/user/${userId}`);
@@ -32,8 +40,15 @@ const Profile = ({ userId, user }) => {
         }
         const favInfo = await axios.get(`/api/fav/user/${userId}`);
         setFavlist(favInfo.data.favorites);
-        //const rvInfo = await axios.get(`/api/`);
-        // setRecentlyViewedList(['0003d5b120']); //replace
+        /*const rvInfo = await axios.get(`/api/recentRecipe`,
+        {
+          selectedUsername: user.username,
+          recipeId: '0003d5b120'
+        });*/
+        //setRecentlyViewedList(['0003d5b120']); //replace
+        console.log("New info: ");
+        console.log(userInfo.data);
+        console.log(userInfo.data.recentlyViewed);
         setRecentlyViewedList(userInfo.data.recentlyViewed);
       }
     }
