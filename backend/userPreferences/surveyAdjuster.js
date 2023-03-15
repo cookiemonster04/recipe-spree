6052ad488d66d894f7927a560e58cb4cafd533a7
AlexanderThaik <ajt.school@gmail.com>
2023-03-15 14:28:29 -0700

    Added small fix to survey adjuster

--- backend/userPreferences/surveyAdjuster.js
+++ backend/userPreferences/surveyAdjuster.js
@@ -5,8 +5,9 @@ import User from '../models/userModel.js';
 import { catchWrap } from "../middleware/errorHandler.js";
 
 
-async function surveyAdjuster(selectedUsername, sets) 
+async function surveyAdjuster(selectedUsername, love, like, dislike, blacklisted) 
 {
+    sets = [love, like, dislike, blacklisted];
     sets.forEach(set =>
         {
             set.forEach(element =>
@@ -44,7 +45,7 @@ async function surveyAdjuster(selectedUsername, sets)
 
 const surveyHandler = catchWrap(async (req, res, next) => {
     const { selectedUsername, sets } = req.body;
-    await surveyAdjuster(selectedUsername, sets);
+    await surveyAdjuster(selectedUsername, love, like, dislike, blacklisted);
     res.status(200).send("Received survey data");
 });
 
