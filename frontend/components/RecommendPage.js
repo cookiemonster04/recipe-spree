3ea1e64c0a7f43b69955969a0d2f8e191c0c7a52
Lortentz <leo_liao2002@outlook.com>
2023-03-16 23:02:53 -0700

    New Recommend feature and bug fixing.
    Add the recommend page. Work.
    Fix minor bug in Profile RecipeCard Search Survey

---
+++ frontend/components/RecommendPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Grid, Typography } from "@mui/material";
+import RecipeCard from "./RecipeCard";
+
+function RecommendPage ({ user }) {
+    const [recipeList, setRecipeList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        async function getRecommend() {
+            try {
+                const response = await axios.post("/api/recipeRecommender", {
+                    selectedUsername: user.username,
+                })
+                setRecipeList(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+                setLoading(false);
+            }
+        }
+        getRecommend();
+    }, [user])
+    // console.log(`Recipe list length ${recipeList.length}`)
+    if (loading) {
+        return <Typography marginTop={2} variant="h6">Loading...</Typography>;
+    }
+    return (
+        <div className="card-container">
+          {recipeList.length > 0 ? (
+            <>
+                <Typography marginTop={2} variant="h6">Recommend recipes for you!</Typography>
+                <Grid container my={2} spacing={2}>
+                {recipeList.slice(0,50).map((recipe, idx) => <RecipeCard recipeId={recipe.id} idx={idx} />)}
+                </Grid>
+            </>
+          ) : (
+            <Typography marginTop={2} variant="h6">
+              No results. Please try to search something else. Please make sure you include ingredients.
+            </Typography>
+          )}
+        </div>
+    );
+};
+
+export default RecommendPage;
--- frontend/routes/Survey.js
+++ frontend/routes/Survey.js
@@ -3,7 +3,7 @@ import KanbanBoard from "../components/KanbanBoard";
 import { Navigate } from "react-router-dom";
 
 const Survey = ({ user, location }) => {
-  console.log(user)
+  // console.log(user)
   if (!user && !location?.state?.user ) {
     return <Navigate to="/login" />;
   } else {
