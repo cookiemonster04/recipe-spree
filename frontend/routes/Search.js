26ca11b3d0f508c4a1c3a0568ac325d318549a2d
Edwin Montalvo <edwinmontavlo123@gmail.com>
2023-03-16 20:38:25 -0700

    Make navbar smaller

--- frontend/routes/Search.js
+++ frontend/routes/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RecipeCard from "../components/RecipeCard"
 import axios from "axios";
 import './Search.css'
